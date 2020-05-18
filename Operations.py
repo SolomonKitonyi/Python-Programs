09a2c402d4513b5c4dbe05ec3a593f447d1ce797
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-05-18 14:28:09 +0300

    Operations in python

---
+++ Operations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 14:22:21 2020
+
+@author: solomon
+"""
+#Operations
+x = 10 
+y = x + 20
+print y
+subtract = x - y
+print "Subtract is equals to ",subtract
