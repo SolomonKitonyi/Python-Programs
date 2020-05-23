6fbd5bc6087e6482b4cd8b4226a5f7085e935a3c
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-05-23 11:43:51 +0300

    A function to find maximum and minimum letter in a string

---
+++ functiontofindmaxand min letter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 11:28:13 2020
+
+@author: solomon
+"""
+
+big = max("Am Solomon a student at Strathmore Uneversity")
+print big
+
+small = min("helloa")
+print small
