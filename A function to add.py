0e7442903f6eaac628ce184ad13b03f48cd5ee8c
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-05-23 12:10:58 +0300

    A function to add two numbers

---
+++ A function to add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 12:06:21 2020
+
+@author: solomon
+"""
+
+def add():
+    x = 10
+    y = 20
+    addition = x + y
+    print "The sum of two numbers is ",addition
+add()
