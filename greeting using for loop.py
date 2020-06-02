a3237913976db89f8279c356f5d6c52ba0a67c7f
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-06-02 15:44:05 +0300

    greeting using for loop

---
+++ greeting using for loop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 15:26:45 2020
+
+@author: solomon
+"""
+
+friends = ["Solomon","Nick", "Brian","Duncan","Lorna"]
+for friend in friends:
+    print "You are good to me ",friend
+print "well"
