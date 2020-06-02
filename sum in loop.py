d911eabee2cd8748507d80fbd9cb5fee8a6983ad
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-06-02 19:13:56 +0300

    Summing numbers using for loops

---
+++ sum in loop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 19:10:28 2020
+
+@author: solomon
+"""
+
+sum = 0
+for num in[23,4,56,66,777,34,34]:
+    sum = sum + num
+    print sum
+print "The total sum is ",sum
