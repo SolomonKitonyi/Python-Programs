e95e6db8ee8cc25670086060088648e8fc016a97
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-06-02 14:51:07 +0300

    Breakout loops

---
+++ Breaking out loop.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 14:45:13 2020
+
+@author: solomon
+"""
+
+while True:
+    line = raw_input(">")
+    if line == "Done":
+     break
+    print line
+print "Hello there you got it"
