8983ee5358b98812593069cad9958fd34892e5df
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-06-02 18:58:21 +0300

    Count in for loop

---
+++ count using for loop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 18:01:54 2020
+
+@author: solomon
+"""
+
+count = 0
+for i in [1,2,3,4,5,6,7,6,89,7]:
+    print count
+    count = count + 1
+print "The count is ",count
