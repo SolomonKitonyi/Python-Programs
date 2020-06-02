e6d2ddd8c41d389708afa72017cce4cdd13e2236
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-06-02 20:53:46 +0300

    Average in python

---
+++ Evarage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 20:46:33 2020
+
+@author: solomon
+"""
+sum = 0
+count = 0
+for i in [12,34,56,66]:
+    sum = sum + i
+    print "The sum so far is ",sum
+    count = count + 1
+    print "The count so far is ", count
+    evarage = sum/count
+print "The evarage is ",evarage
