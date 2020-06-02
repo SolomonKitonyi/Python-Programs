4326404efee21b0efebd24f4bcad52558629a6de
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-06-02 14:38:48 +0300

    While loop example

---
+++ whileloop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 14:31:45 2020
+
+@author: solomon
+"""
+
+n = 10
+while n > 0:
+    print "Am in position ",n
+    n = n - 1
+print "Am done"
