9c054c80b5a19b74998fb4fcdeb692fb3a71add0
SolomonKitonyi <kitonyisolomon@gmail.com>
2020-07-27 12:59:50 +0300

    add a program to convert upper case to lower case

---
+++ convertstringtosmall letters.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 12:55:12 2020
+
+@author: solomon
+"""
+
+Name = raw_input("Enter your name in capital letter: ");
+namesmall = Name.lower();
+print "Your name in small letter is " + namesmall;
